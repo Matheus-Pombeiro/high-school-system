ec849f412c525cf5204e61f70c2953772a9b0485
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-11 14:25:42 -0300

    feat: navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,26 @@
+import { NavLink } from "react-router-dom";
+import HeaderLink from "../HeaderLink";
+
 const Header = () => {
     return (
-        <header>
-            
+        <header className="box-border py-3 px-5 bg-blue-500 flex justify-between items-center">
+            <NavLink to="/" >
+                <img 
+                    src="/assets/logo.png" 
+                    alt="Logo" 
+                    title="Home"
+                    className="h-14"
+                />
+            </NavLink>
+
+            <nav className="box-border flex justify-between items-center gap-3">
+                <HeaderLink to="/" >
+                    Home
+                </HeaderLink>
+                <HeaderLink to="/new-student" >
+                    New Student
+                </HeaderLink>
+            </nav>
         </header>
     );
 };
