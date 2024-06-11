5b5a0d4bc8445bd7a509e024e607a17753cc9621
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-11 14:25:16 -0300

    feat: HeaderLink component

---
+++ src/components/HeaderLink/index.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+
+const HeaderLink = ({children, to}) => {
+    return (
+        <NavLink
+            className={({ isActive }) => `
+                text-white text-xl font-medium pb-0.5
+                ${isActive ? "border-b-2 border-red-500" : ""}
+            `}
+            to={to}
+        >
+            {children}
+        </NavLink>
+    );
+};
+
+export default HeaderLink;
