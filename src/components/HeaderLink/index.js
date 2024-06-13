b704f997f3c98e17382b9928bd5cee64437d51f6
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-13 14:21:45 -0300

    refactor: text size

--- src/components/HeaderLink/index.js
+++ src/components/HeaderLink/index.js
@@ -4,7 +4,7 @@ const HeaderLink = ({children, to}) => {
     return (
         <NavLink
             className={({ isActive }) => `
-                text-white text-xl font-medium pb-0.5
+                text-white text-2xl font-medium pb-0.5
                 ${isActive ? "border-b-2 border-red-500" : ""}
             `}
             to={to}
