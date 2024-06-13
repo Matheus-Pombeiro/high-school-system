fdfa1989b17d72af55233fa572679c0a6f419c79
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-13 16:27:06 -0300

    feat: toRegisterStudent function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NewStudent from "./pages/NewStudent";
 import DefaultPage from "./components/DefaultPage";
 
 const App = () => {
+  const [students, setStudents] = useState([]);
+
+  console.log(students)
+
   return (
     <BrowserRouter>
       <Header />
@@ -13,7 +19,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<DefaultPage />}>
           <Route index element={<Home />} />
-          <Route path="new-student" element={<NewStudent />} />
+          <Route 
+            path="new-student" 
+            element={<NewStudent 
+              toRegisterStudent={student => setStudents([...students, student])}
+            />} 
+          />
         </Route>
 
         <Route path="*" element={<h1>Not Found Page</h1>} />
