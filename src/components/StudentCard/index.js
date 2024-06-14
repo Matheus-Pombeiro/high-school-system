33de12564222a02621d584981a99e7b09907b3b5
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-14 14:04:09 -0300

    feat: delete student onClick

--- src/components/StudentCard/index.js
+++ src/components/StudentCard/index.js
@@ -1,6 +1,15 @@
-const StudentCard = ({ student }) => {
+import { AiFillCloseCircle } from "react-icons/ai";
+
+const StudentCard = ({ student, toDeleteStudent }) => {
     return (
-        <div className="w-64 h-72 bg-white box-border border border-blue-500 rounded-lg flex flex-col">
+        <div className="w-64 h-72 bg-white box-border border border-blue-500 rounded-lg flex flex-col relative">
+            <AiFillCloseCircle
+                color="red"
+                size={25} 
+                className="absolute -top-4 -right-4 cursor-pointer"
+                onClick={() => toDeleteStudent(student.id)}
+            />
+
             <div className="w-full h-32 bg-blue-500 flex justify-center">
                 <img 
                     src={student.profilePhoto}
@@ -9,6 +18,7 @@ const StudentCard = ({ student }) => {
                     className="size-28 rounded-full relative top-2/4"
                 />
             </div>
+
             <div className="my-auto space-y-4 text-center">
                 <h3 className="mt-8 text-lg text-red-500 font-semibold">{student.name}</h3>
                 <h4>{student.currentSubject}</h4>
