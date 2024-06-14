f5bb2ffa0978f3fed130a179d22ed994aa33b9fe
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-14 13:44:32 -0300

    refactor: student card moved to its own component

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,4 +1,5 @@
 import { FaSortAlphaDown, FaSortAlphaDownAlt } from "react-icons/fa";
+import StudentCard from "../../components/StudentCard";
 
 const Students = ({ students }) => {
     // Declare an object that set the buttons' properties
@@ -26,20 +27,7 @@ const Students = ({ students }) => {
 
                 <section className="container-cards side-container">
                     {students.map(student => (
-                        <div className="w-64 h-72 bg-white box-border border border-blue-500 rounded-lg flex flex-col">
-                            <div className="w-full h-32 bg-blue-500 flex justify-center">
-                                <img 
-                                    src={student.profilePhoto}
-                                    alt={student.name}
-                                    title={student.name}
-                                    className="size-28 rounded-full relative top-2/4"
-                                />
-                            </div>
-                            <div className="my-auto space-y-4 text-center">
-                                <h3 className="mt-8 text-lg text-red-500 font-semibold">{student.name}</h3>
-                                <h4>{student.currentSubject}</h4>
-                            </div>
-                        </div>
+                        <StudentCard student={student} />
                     ))}
 
                 </section>
