70a378fecc09e82b2ea8282ed3719fc7f2f50207
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-16 13:51:15 -0300

    feat: link to Student page and animation stylization

--- src/components/StudentCard/index.js
+++ src/components/StudentCard/index.js
@@ -1,36 +1,38 @@
+import { Link } from "react-router-dom";
+
 import { AiFillCloseCircle } from "react-icons/ai";
 
 const StudentCard = ({ student, toDeleteStudent }) => {
     return (
-        <div 
-            className="w-64 h-72 bg-white box-border border border-blue-500 rounded-lg flex flex-col relative"    
-        >
-            <AiFillCloseCircle
-                color="red"
-                size={25} 
-                className="absolute -top-4 -right-4 cursor-pointer"
-                onClick={() => toDeleteStudent(student.id)}
-            />
-
-            <div className="w-full h-32 bg-blue-500 flex justify-center">
-                {student.profilePhoto 
-                    ? (
-                        <img 
-                            src={student.profilePhoto}
-                            alt={student.name}
-                            title={student.name}
-                            className="size-28 rounded-full relative top-2/4"
-                        />
-                    )
-                    : ""
-                }
+        <Link to={`/student/${student.id}`} >
+            <div
+                className="w-64 h-72 bg-white box-border border border-blue-500 rounded-lg flex flex-col relative transition-all ease-in-out duration-200 hover:-translate-y-3"
+            >
+                <AiFillCloseCircle
+                    color="red"
+                    size={25}
+                    className="absolute -top-4 -right-4 cursor-pointer"
+                    onClick={() => toDeleteStudent(student.id)}
+                />
+                <div className="w-full h-32 bg-blue-500 flex justify-center">
+                    {student.profilePhoto
+                        ? (
+                            <img
+                                src={student.profilePhoto}
+                                alt={student.name}
+                                title={student.name}
+                                className="size-28 rounded-full relative top-2/4"
+                            />
+                        )
+                        : ""
+                    }
+                </div>
+                <div className="my-auto space-y-4 text-center">
+                    <h3 className="mt-8 text-lg text-red-500 font-semibold">{student.name}</h3>
+                    <h4>{student.currentSubject}</h4>
+                </div>
             </div>
-
-            <div className="my-auto space-y-4 text-center">
-                <h3 className="mt-8 text-lg text-red-500 font-semibold">{student.name}</h3>
-                <h4>{student.currentSubject}</h4>
-            </div>
-        </div>
+        </Link>
     );
 };
 
