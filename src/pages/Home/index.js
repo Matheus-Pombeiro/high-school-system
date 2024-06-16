bf35f21e38755513f52066ff513a1fad1e4157ee
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-06-16 15:49:48 -0300

    refactor: Banner extracted to its own component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,13 @@
 import { useTranslation } from "react-i18next";
 
+import Banner from "../../components/Banner";
+
 const Home = () => {
     const { t } = useTranslation()
 
     return (
         <>
-            <div 
-                className="h-60 md:h-72 bg-cover bg-center flex justify-center items-center"
-                style={{ backgroundImage: "url(/assets/main-bg.jpg)" }}
-            >
-                <h1 className="text-white h1">High School System</h1>
-            </div>
+            <Banner />
 
             <section className="container-text">
                 <article className="space-y-4">
